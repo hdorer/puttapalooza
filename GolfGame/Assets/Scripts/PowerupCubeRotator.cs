514a62aad30a8c7545ba5e0d6b85cf26104266c9
Harry Dorer <henry.dorer@mymail.champlain.edu>
2023-02-08 00:14:21 -0500

    Set up powerup testing scene

---
+++ GolfGame/Assets/Scripts/PowerupCubeRotator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupCubeRotator : MonoBehaviour {
+    private const float rotationSpeed = 50f;
+    private const float yMoveScalar = 0.3f;
+    private float startingY;
+
+    private void Start() {
+        startingY = transform.position.y;
+    }
+
+    private void Update() {
+        float y = startingY + Mathf.Sin(Time.timeSinceLevelLoad) * yMoveScalar;
+
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.World);
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+}
