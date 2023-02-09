68a9dbd654d116641be2cd3f4cf90ddfb1ff05ef
Harry Dorer <henry.dorer@mymail.champlain.edu>
2023-02-09 17:21:20 -0500

    Revert a8d227d5 - d1235b77 Rethinking how I want to handle the magnet powerup If I did this correctly, the working directory at this commit should be the same as the working directory at 2d7eae2d.

---
+++ GolfGame/Assets/Scripts/Powerup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Powerup : ScriptableObject {
+    [SerializeField] private string powerupName;
+    [SerializeField] private string description;
+    public string Name { get { return powerupName; } }
+    
+    public abstract void use();
+}
--- GolfGame/Assets/Scripts/PowerupPickup.cs
+++ GolfGame/Assets/Scripts/PowerupPickup.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Powerups;
 using UnityEngine;
 
 public class PowerupPickup : MonoBehaviour {
