f98778b3e0b1f87b84ebdc4dc886073f2bfdad29
Harry Dorer <henry.dorer@mymail.champlain.edu>
2023-02-15 14:35:27 -0500

    Updated scene with Cinemachine virtual camera

--- GolfGame/Assets/Scripts/BallCameraController.cs
+++ GolfGame/Assets/Scripts/BallCameraController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CameraController : MonoBehaviour {
+public class BallCameraController : MonoBehaviour {
     [SerializeField] private Transform ball;
 
     private bool rotating = false;
