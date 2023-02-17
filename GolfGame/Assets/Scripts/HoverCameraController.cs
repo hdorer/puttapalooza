9828b19a8a29c887750db428c91cbb5f629f2f9f
Harry Dorer <henry.dorer@mymail.champlain.edu>
2023-02-17 17:38:58 -0500

    Added basic hover cam movement

--- GolfGame/Assets/Scripts/HoverCameraController.cs
+++ GolfGame/Assets/Scripts/HoverCameraController.cs
@@ -10,21 +10,36 @@ public class HoverCameraController : MonoBehaviour {
     [SerializeField] private float movementSpeed = 5.0f;
 
     [SerializeField] private InputAction movementInput;
+    Vector2 movement;
     
 
     private void OnEnable() {
         movementInput.Enable();
+
+        movementInput.performed += updateMoveVector;
+        movementInput.canceled += updateMoveVector;
+    }
+
+    private void Update() {
+        transform.position += transform.right * movement.x;
+        transform.position += transform.up * movement.y;
     }
 
     private void OnDisable() {
         movementInput.Disable();
+
+        movementInput.performed -= updateMoveVector;
+        movementInput.canceled -= updateMoveVector;
     }
 
     public void resetTransform() {
-        // transform.position = new Vector3(ballCameraAnchor.position.x, transform.position.y, ballCameraAnchor.position.z);
-        // transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, ballCameraAnchor.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-        Debug.Log("OnCameraLive");
+        transform.position = new Vector3(ballCameraAnchor.position.x, transform.position.y, ballCameraAnchor.position.z);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, ballCameraAnchor.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 
-    
+    private void updateMoveVector(InputAction.CallbackContext context) {
+        Vector2 inputValue = context.ReadValue<Vector2>();
+
+        movement = inputValue;
+    }
 }
