8dd01a08d49e56cdf2f6f24a2b1533518544f09c
nicolas.delbue <nicolas.delbue@mymail.champlain.edu>
2023-03-06 10:29:36 -0500

    Added UI + Ice
    Added UI in menu, and ice physics material

--- GolfGame/Assets/NickStuff/Scripts/EventSystemButtons.cs
+++ GolfGame/Assets/NickStuff/Scripts/EventSystemButtons.cs
@@ -8,23 +8,39 @@ public class EventSystemButtons : MonoBehaviour
 {   
     [SerializeField] private GameObject MenuPanel;
     [SerializeField] private GameObject SettingPanel;
+    [SerializeField] private GameObject ControlPanel;
+    [SerializeField] private GameObject PlayerPanel;
     void Start()
     {
         if(SceneChange.CheckScene() == 0)
         {
             MenuPanel.SetActive(true);
             SettingPanel.SetActive(false);
+            ControlPanel.SetActive(false);
+            PlayerPanel.SetActive(false);
         }
     }
     public void SwitchToSettings()
     {
         SettingPanel.SetActive(true);
         MenuPanel.SetActive(false);
+        ControlPanel.SetActive(false);
     }
     public void SwitchToMain()
     {
         MenuPanel.SetActive(true);
         SettingPanel.SetActive(false);
+        PlayerPanel.SetActive(false);
+    }
+    public void SwitchToControl()
+    {
+        SettingPanel.SetActive(false);
+        ControlPanel.SetActive(true);
+    }
+    public void SwitchToPlayer()
+    {
+        PlayerPanel.SetActive(false);
+        MenuPanel.SetActive(false);
     }
     public void SwitchScene(int sceneNum)
     {
