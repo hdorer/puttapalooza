427aa2c1087ae73c8f7fce4e24bf724bf6d3eb8d
Harry Dorer <henry.dorer@mymail.champlain.edu>
2023-03-07 16:45:08 -0500

    The generation code is now mostly working as intended

--- GolfGame/Assets/Scripts/UI/FullScoreDisplayGenerator.cs
+++ GolfGame/Assets/Scripts/UI/FullScoreDisplayGenerator.cs
@@ -3,14 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
-using UnityEngine.Assertions.Must;
 
 public class FullScoreDisplayGenerator : MonoBehaviour {
     [Header("Prefabs")]
     [SerializeField] private TextMeshProUGUI playerLabelPrefab;
     [SerializeField] private TextMeshProUGUI holeLabelPrefab;
-    [SerializeField] private TextMeshProUGUI scoreSlotPrefab;
+    [SerializeField] private TextMeshProUGUI scoreLabelPrefab;
 
     [Header("Scene Transforms")]
     [SerializeField] private RectTransform scoreDisplayPanel;
@@ -25,6 +23,8 @@ public class FullScoreDisplayGenerator : MonoBehaviour {
         clearTexts();
         generatePlayerLabels();
         generateHoleLabels();
+        generateScoreLabels();
+        // resizePanel();
     }
 
     private void clearTexts() {
@@ -41,9 +41,9 @@ public class FullScoreDisplayGenerator : MonoBehaviour {
         for(int i = 0; i < players; i++) {
             Vector3 position = playerLabelStartingPoint.position - new Vector3(0, playerLabelPrefab.rectTransform.rect.height * i, 0);
             TextMeshProUGUI text = Instantiate(playerLabelPrefab, position, Quaternion.identity);
-            text.rectTransform.parent = scoreDisplayPanel;
-            text.text = "Player " + i;
-            text.gameObject.name = "Player" + i + "Label";
+            text.rectTransform.SetParent(scoreDisplayPanel);
+            text.text = "Player " + (i + 1);
+            text.gameObject.name = "Player" + (i + 1) + "Label";
         }
     }
 
@@ -52,15 +52,52 @@ public class FullScoreDisplayGenerator : MonoBehaviour {
         for(i = 0; i < holes; i++) {
             Vector3 position = holeLabelStartingPoint.position + new Vector3(holeLabelPrefab.rectTransform.rect.width * i, 0, 0);
             TextMeshProUGUI text = Instantiate(holeLabelPrefab, position, Quaternion.identity);
-            text.rectTransform.parent = scoreDisplayPanel;
-            text.text = i.ToString();
-            text.gameObject.name = "Hole" + i + "Label";
+            text.rectTransform.SetParent(scoreDisplayPanel);
+            text.text = (i + 1).ToString();
+            text.gameObject.name = "Hole" + (i + 1) + "Label";
         }
 
         Vector3 totalPosition = holeLabelStartingPoint.position + new Vector3(holeLabelPrefab.rectTransform.rect.width * i, 0, 0);
         TextMeshProUGUI totalText = Instantiate(holeLabelPrefab, totalPosition, Quaternion.identity);
-        totalText.rectTransform.parent = scoreDisplayPanel;
+        totalText.rectTransform.SetParent(scoreDisplayPanel);
         totalText.text = "T";
         totalText.gameObject.name = "TotalScoreLabel";
+    }
+
+    private void generateScoreLabels() {
+        for(int i = 0; i < players; i++) {
+            int j;
+            for(j = 0; j < holes; j++) {
+                float posX = holeLabelStartingPoint.position.x + holeLabelPrefab.rectTransform.rect.width * j;
+                float posY = playerLabelStartingPoint.position.y - playerLabelPrefab.rectTransform.rect.height * i;
+                Vector3 position = new Vector3(posX, posY, 0);
+
+                TextMeshProUGUI text = Instantiate(scoreLabelPrefab, position, Quaternion.identity);
+                text.rectTransform.SetParent(scoreDisplayPanel);
+                text.gameObject.name = "Player" + (i + 1) + "Hole" + (j + 1);
+            }
+
+            float totalPosX = holeLabelStartingPoint.position.x + holeLabelPrefab.rectTransform.rect.width * j;
+            float totalPosY = playerLabelStartingPoint.position.y - playerLabelPrefab.rectTransform.rect.height * i;
+            Vector3 totalPosition = new Vector3(totalPosX, totalPosY, 0);
+
+            TextMeshProUGUI totalText = Instantiate(scoreLabelPrefab, totalPosition, Quaternion.identity);
+            totalText.rectTransform.SetParent(scoreDisplayPanel);
+            totalText.gameObject.name = "Player" + (i + 1) + "Total";
+        }
+    }
+
+    private void resizePanel() {
+        // TODO: this doesn't work and needs to be fixed
+        
+        float hPadding = playerLabelStartingPoint.position.x - playerLabelPrefab.rectTransform.rect.width / 2;
+        float vPadding = holeLabelStartingPoint.position.y - holeLabelPrefab.rectTransform.rect.width / 2;
+
+        float panelWidth = (holeLabelStartingPoint.position.x + holeLabelPrefab.rectTransform.rect.width * (holes - 1) + hPadding);
+        float panelHeight = (playerLabelStartingPoint.position.y + playerLabelPrefab.rectTransform.rect.height * (players - 1) + vPadding);
+
+        scoreDisplayPanel.sizeDelta = new Vector2(panelWidth, panelHeight);
+
+        scoreDisplayPanel.position = new Vector3(transform.position.x, transform.position.y, 0);
     }
 }
