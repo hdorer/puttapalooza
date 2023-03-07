9905ca15e7cbfa738d5d30d2b02cae497b49b4e6
Harry Dorer <henry.dorer@mymail.champlain.edu>
2023-03-07 17:21:13 -0500

    Pressing Tab now shows the score display mid-level

--- GolfGame/Assets/Scripts/UI/FullScoreDisplay.cs
+++ GolfGame/Assets/Scripts/UI/FullScoreDisplay.cs
@@ -2,10 +2,28 @@ using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FullScoreDisplay : MonoBehaviour {
     private TextMeshProUGUI[,] scoreLabels;
     private TextMeshProUGUI[] totalScoreLabels;
+
+    [SerializeField] GameObject scoreDisplayPanel;
+    [SerializeField] private InputAction showScoreDisplayInput;
+
+    private void OnEnable() {
+        showScoreDisplayInput.Enable();
+
+        showScoreDisplayInput.performed += showScoreDisplay;
+        showScoreDisplayInput.canceled += hideScoreDisplay;
+    }
+
+    private void OnDisable() {
+        showScoreDisplayInput.Disable();
+
+        showScoreDisplayInput.performed -= showScoreDisplay;
+        showScoreDisplayInput.canceled -= hideScoreDisplay;
+    }
 
     public void initializeArrays(int players, int holes) {
         scoreLabels = new TextMeshProUGUI[players, holes];
@@ -18,5 +36,21 @@ public class FullScoreDisplay : MonoBehaviour {
 
     public void addScoreLabel(int player, TextMeshProUGUI scoreLabel) {
         totalScoreLabels[player] = scoreLabel;
+    }
+
+    private void showScoreDisplay(InputAction.CallbackContext context) {
+        showScoreDisplay();
+    }
+
+    private void showScoreDisplay() {
+        scoreDisplayPanel.SetActive(true);
+    }
+
+    private void hideScoreDisplay(InputAction.CallbackContext context) {
+        hideScoreDisplay();
+    }
+
+    private void hideScoreDisplay() {
+        scoreDisplayPanel.SetActive(false);
     }
 }
