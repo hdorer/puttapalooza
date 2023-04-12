781ce306548cbbced06e2221e0ca31989bf69146
Nicolas D <nicolas.delbue@mymail.champlain.edu>
2023-04-12 18:56:03 -0400

    Added audio and base sound code

---
+++ GolfGame/Assets/NickStuff/Scripts/EventSystemSounds.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemSounds : MonoBehaviour
+{
+    [SerializeField] private AudioClip ballSelect;
+    [SerializeField] private AudioClip playerJoin;
+    [SerializeField] private AudioClip menuButton;
+
+    public void playInputAudio(AudioClip audio)
+    {
+
+    }
+    
+}
