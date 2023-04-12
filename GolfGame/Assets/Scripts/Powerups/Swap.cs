beab017cc08af9e712e509f411ff527b4b1f3828
Harry Dorer <41219697+hcdorer@users.noreply.github.com>
2023-04-12 16:13:56 -0400

    I don't know what's going on anymore

--- GolfGame/Assets/Scripts/Powerups/Swap.cs
+++ GolfGame/Assets/Scripts/Powerups/Swap.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Swap", menuName = "ScriptableObjects/Powerups/Swap")]
 public class Swap : Powerup {
     public override void use(PlayerPowerups ball) {
+        if(GameManager.NumPlayers < 2) {
+            return;
+        }
+
         GameObject target = LevelManager.getRandomPlayer(ball.GetComponent<PlayerTurn>().Id);
         
         Vector3 position = ball.transform.position;
