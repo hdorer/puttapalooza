fd4c9b0bf4d7c4bb27563911ef388e03bb21cf18
nicolas.delbue <nicolas.delbue@mymail.champlain.edu>
2023-04-21 10:44:02 -0400

    Adding More Audio

--- GolfGame/Assets/NickStuff/Scripts/PlayerAudio.cs
+++ GolfGame/Assets/NickStuff/Scripts/PlayerAudio.cs
@@ -7,6 +7,11 @@ public class PlayerAudio : MonoBehaviour
 {
     [SerializeField] AudioSource audioPlayer;
     [SerializeField] private AudioClip ballHit;
+    [SerializeField] private AudioClip ballHitWall;
+    [SerializeField] private AudioClip ballLandGround;
+    [SerializeField] private AudioClip ballLandSand;
+    [SerializeField] private AudioClip ballLandIce;
+    [SerializeField] private AudioClip powerUpUse;
 
     private void Start() {
         audioPlayer.volume = GameManager.SfxVolume;
@@ -14,5 +19,20 @@ public class PlayerAudio : MonoBehaviour
 
     public void playBallHit() {
         audioPlayer.PlayOneShot(ballHit);
+    }
+    public void playBallWall() {
+        audioPlayer.PlayOneShot(ballHitWall);
+    }
+    public void playBallGround() {
+        audioPlayer.PlayOneShot(ballLandGround);
+    }
+    public void playBallSand() {
+        audioPlayer.PlayOneShot(ballLandSand);
+    }
+    public void playBallIce() {
+        audioPlayer.PlayOneShot(ballLandIce);
+    }
+    public void playPowerupUse() {
+        audioPlayer.PlayOneShot(powerUpUse);
     }
 }
